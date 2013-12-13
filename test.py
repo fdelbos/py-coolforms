c47ce8a6a73324edee64ef02aac931c01ee1c5ed
Frederic Delbos <fred.delbos@gmail.com>
2013-12-13 06:30:29 +0100

    basic tests

---
+++ test.py
@@ -0,0 +1,27 @@
+# 
+# test.py
+# 
+# Created by Frederic DELBOS - fred.delbos@gmail.com on Dec 13 2013.
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.txt', which is part of this source code package.
+# 
+
+import unittest
+import coolforms as cf
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def test_make_form(self):
+        f = cf.CoolForm("form", "/truc")
+        f.page("First Page", "this is my first page")
+        f.line()
+        f.field("f1", "text", "a text field", 1, "some help")
+        f.field("f2", "text", "a text field", 1, "some help")
+        f.line()
+        f.field("email", "text", "an email field", 1)
+        f.validator("email", "invalid email")
+        print f.json(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
