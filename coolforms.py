9954340b38d0e37bac4c1eaebe0fc1b883f1e5c5
Frederic Delbos <fred.delbos@gmail.com>
2013-12-13 06:58:44 +0100

    modified for groc

--- coolforms.py
+++ coolforms.py
@@ -1,10 +1,10 @@
-# 
-# coolforms.py
-# 
-# Created by Frederic DELBOS - fred.delbos@gmail.com on Dec 13 2013.
-# This file is subject to the terms and conditions defined in
-# file 'LICENSE.txt', which is part of this source code package.
-# 
+## 
+## coolforms.py
+## 
+## Created by Frederic DELBOS - fred.delbos@gmail.com on Dec 13 2013.
+## This file is subject to the terms and conditions defined in
+## file 'LICENSE.txt', which is part of this source code package.
+## 
 
 import json
 
